d2bde2857356c47549b6aa702b56b8d03c4021d9
Megugo <Buhlou89@gmail.com>
2020-03-31 18:55:29 +0300

    Add files via upload

---
+++ lab1/lab1.py
@@ -0,0 +1,15 @@
+import csv
+cdr=[]
+with open('data.csv') as file:
+	reader = csv.reader(file)
+	for row in reader:
+		cdr.append(row)
+cd=-20 
+sn=0
+
+for i in range(1,len(cdr)): 
+	if '933156729' in cdr[i][cdr[0].index('msisdn_origin')]: 
+		cd+=float(cdr[i][cdr[0].index('call_duration')]) 
+		sn+=float(cdr[i][cdr[0].index('sms_number')]) 
+print (cd*2 + sn*2)
+input()
